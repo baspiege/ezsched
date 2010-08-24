4ddd74775077052f4bb6d2670d9c61e73c9b16ca
spiegel121 <spiegel121@c5ed28fc-0fc1-05ea-d8f4-389ebfd25d61>
2010-08-24 02:05:03 +0000

    Update to app engine 1.3.6.

--- bin/dev_appserver.cmd
+++ bin/dev_appserver.cmd
@@ -1,6 +1,5 @@
 set JAVA_HOME=c:\Program Files\Java\jdk1.6.0_15\bin
 
-"%JAVA_HOME%\java" -cp "%~dp0\..\lib\appengine-tools-api.jar" ^
+"%JAVA_HOME%\java" -cp "c:\dev\appengine-java-sdk-1.3.6\lib\appengine-tools-api.jar" ^
     com.google.appengine.tools.KickStart ^
        com.google.appengine.tools.development.DevAppServerMain %*
-
--- bin/upload.bat
+++ bin/upload.bat
@@ -1,2 +1,2 @@
 set JAVA_HOME=c:\Program Files\Java\jdk1.6.0_15\bin
-C:\dev\appengine-java-sdk-1.3.1\sched_bin\appcfg.cmd update C:\dev\sched\war
+C:\dev\appengine-java-sdk-1.3.6\sched_bin\appcfg.cmd update C:\dev\sched\war

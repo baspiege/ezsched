eaeca505b0d7231c3709c3f029a9cd4fab6cbe13
spiegel121 <spiegel121@c5ed28fc-0fc1-05ea-d8f4-389ebfd25d61>
2010-09-09 02:55:12 +0000

    Update command for uploading.

--- bin/appcfg.cmd
+++ bin/appcfg.cmd
@@ -1,2 +1,2 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_15\bin
-"%JAVA_HOME%\java" -cp "%~dp0\..\lib\appengine-tools-api.jar" com.google.appengine.tools.admin.AppCfg %*
+"%JAVA_HOME%\java" -cp "c:\dev\appengine-java-sdk-1.3.6\lib\appengine-tools-api.jar" com.google.appengine.tools.admin.AppCfg %*
